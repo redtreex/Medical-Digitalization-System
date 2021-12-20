fc8c65f7ec00699cf64478fe39a1e0421c6ded3e
tanviranindo <tanviranindo@gmail.com>
2021-12-21 01:43:11 +0600

    Initial Commit

---
+++ src/main/java/com/project/medicaldigitalization/MedicalDigitalizationApplication.java
@@ -0,0 +1,13 @@
+package com.project.medicaldigitalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDigitalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDigitalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/medicaldigitalization/MedicalDigitalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medicaldigitalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDigitalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
